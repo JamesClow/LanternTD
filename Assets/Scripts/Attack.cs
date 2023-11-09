f0197d55811668fdcd8c1ded851cb63a6f3e6313
Sam Kern <samkern108@gmail.com>
2023-11-09 12:50:58 -0500

    Some basic thangs

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public attacksPerSecond = 1;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
